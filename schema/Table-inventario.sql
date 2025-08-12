493c0abcbc0e924bf66c3f5124cf04281ef940d8
Pedro Ortega <pedlui20@gmail.com>
2025-08-12 12:48:46 +0000

    estructura de tablas

---
+++ schema/Table-inventario.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `neptuno-468718.anime.inventario_movimientos` (
+  id_movimiento INT64,
+  id_producto INT64,
+  tipo_movimiento STRING,
+  cantidad INT64,
+  fecha_movimiento DATE
+);
---
+++ schema/Table-proveedores.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `neptuno-468718.anime.proveedores` (
+  id_proveedor INT64,
+  nombre STRING,
+  contacto STRING,
+  telefono STRING,
+  pais STRING
+);
